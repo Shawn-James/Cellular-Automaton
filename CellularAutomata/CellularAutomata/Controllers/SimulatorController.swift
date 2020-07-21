dfe28f0352134602072978d24407b9d75b969ed2
Shawn James <shawn.james@me.com>
2020-07-21 19:26:24 -0400

    Open to a new ViewController called SimulatorController

---
+++ CellularAutomata/CellularAutomata/Controllers/SimulatorController.swift
@@ -0,0 +1,30 @@
+//
+//  SimulatorController.swift
+//  CellularAutomata
+//
+//  Created by Shawn James on 7/21/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+import UIKit
+
+class SimulatorController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
