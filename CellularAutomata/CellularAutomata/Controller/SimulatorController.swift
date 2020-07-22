5eb0f7c0301c3755decf5be343f267f854bb34a0
Shawn James <shawn.james@me.com>
2020-07-21 23:07:33 -0400

    Add MARK comments

--- CellularAutomata/CellularAutomata/Controller/SimulatorController.swift
+++ CellularAutomata/CellularAutomata/Controller/SimulatorController.swift
@@ -4,11 +4,17 @@
 import UIKit
 
 class SimulatorController: UIViewController {
+    // MARK: - Properties
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
     }
+    
+    // MARK: - Handlers
+    
+    // MARK: - Helper Methods
 
 }
 
